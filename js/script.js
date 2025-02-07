19663595a2f12dddb0864a8162fe642018d5b86a
Reidner Willians Pacheco Peres <pacheco195920@gmail.com>
2025-02-06 22:54:30 -0300

    script

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
 function adicionarProduto() {
+
+    //recebe o vallor do input do usuário
+    let inputCompra = document.getElementById("inputCompra");
+    let compra = inputCompra.value;
+
+    //cria novo item (li) e insere na lista de compras) (ul)
+    let listaDeCompras = document.getElementById("listaDeCompras");
+    let novaCompra = document.createElement("li");
+    novaCompra.textContent = compra;
+    listaDeCompras.appendChild(novaCompra);
+
+    //Mensagem de compra adicionada com sucesso
     let mensagem = "Produto adicionado com sucesso!"
     document.getElementById("mensagem").textContent = mensagem;
+    
+    //Limpa o campo de input do usuário
+    inputCompra.value = "";
 }
 
